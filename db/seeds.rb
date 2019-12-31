657bc6ca9f293b1f3c762d5c027a0bdf750acd3c
Anna Dybas <a.m.dybas@gmail.com>
2019-12-31 09:03:58 -0500

    pokemon name api to seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,34 @@
-# require 'rest-client'
-# require 'json'
- 
-# pokemon = RestClient.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-
-# puts JSON.parse(pokemon)
+require 'rest-client'
+require 'json'
+require 'pry'
 
 Pokemon.destroy_all
-Trainer.destroy_all
-Type.destroy_all
 
-squirtle = Pokemon.find_or_create_by(name: "Squirtle", type_id: 1, move: "Bubble", max_hp: 50)
-shelldar = Pokemon.find_or_create_by(name: "Shelldar", type_id: 1, move: "Water Gun", max_hp: 70)
-tentacruel = Pokemon.find_or_create_by(name: "Tentacruel", type_id: 1, move: "Whip", max_hp: 100)
-froakie = Pokemon.find_or_create_by(name: "Froakie", type_id: 1, move: "Scald", max_hp: 50)
+def get_pokemon_array
+    all_pokemon = RestClient.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+    pokemon_hash = JSON.parse(all_pokemon)
+    pokemon_hash["results"]
+end
 
-bulbasaur = Pokemon.find_or_create_by(name: "Bulbasaur", type_id: 2, move: "Wine Whip", max_hp: 50)
-snivy = Pokemon.find_or_create_by(name: "Snivy", type_id: 2, move: "Leaf Tornado", max_hp: 70)
-grookey = Pokemon.find_or_create_by(name: "Grookey", type_id: 2, move: "Grass Knot", max_hp: 100)
-meganium = Pokemon.find_or_create_by(name: "Meganium", type_id: 2, move: "Petal Dance", max_hp: 105)
+def get_pokemon
+    get_pokemon_array.map do |pokemon|
+        name = pokemon["name"]
+        Pokemon.create(name: name)
+        
+        # url = pokemon["url"]
+        # pokemon_url = RestClient.get(url)
+        # pokemon_url_hash = JSON.parse(pokemon_url)
+        # types = pokemon_url_hash["types"].map do |array|
+        #     array["type"]["name"]
+        # end
+        # max_ep = pokemon_url_hash["base_experience"] * 4
+        
+        # moves = pokemon_url_hash["moves"].map do |array|
+        #     array["move"]["name"]
+        #     array["move"]["url"]
+        # end
+    end
+end
 
-charmander = Pokemon.find_or_create_by(name: "Charmander", type_id: 3, move: "Ember", max_hp: 50)
-magmar = Pokemon.find_or_create_by(name: "Magmar", type_id: 3, move: "Flame Thrower", max_hp: 70)
-slugma = Pokemon.find_or_create_by(name: "Slugma", type_id: 3, move: "Incinerate", max_hp: 100)
-ponyta = Pokemon.find_or_create_by(name: "Ponyta", type_id: 3, move: "Fire-Spin", max_hp: 50)
+get_pokemon
+
