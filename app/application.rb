915364a2893c83d2b15aeb5712800067a03f7001
github username <github email address>
2019-12-31 11:32:20 -0500

    refined initial welcome method

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,7 @@
 
 require 'pry'
+require "tty-prompt"
+prompt = TTY::Prompt.new
 
     def clear_screen
         system"clear"
@@ -10,16 +12,21 @@ require 'pry'
         puts "Welcome to Pokemon Battler"
         puts "Please enter your name!"
         username = gets.chomp
-        puts "Hello #{username}! Welcome to the world of pokémon! My name is Oak! People call me the pokémon Prof! This world is inhabited by creatures called pokémon! For some people, pokémon are pets. Others use them for fights. Myself...I study pokémon as a profession."
+        puts "Hi #{username}! Welcome to the world of pokémon! My name is Professor Oak! People call me the pokémon Professor! This world is inhabited by creatures called pokémon! For some people, pokémon are pets. Others use them for fights. Myself...I study pokémon as a profession."
         puts "Are you here to test your skills battling Pokemon?"
         puts "Please type Yes or No"
         battle_option = gets.chomp.downcase
         if battle_option == "no"
         exit
         else
-        puts "Let's battle!"
+        puts "Let's battle! But first, you will need to choose a Pokemon!" 
         end
     end
+
+    def choose_pokemon
+    end
+
+
 
    
         
