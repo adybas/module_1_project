3d31eb4262c6619d233531cca029276209312ef0
github username <github email address>
2019-12-31 11:11:38 -0500

    welcome method established. Can accept user input and exit application if user chooses to not battle

--- app/application.rb
+++ app/application.rb
@@ -1,2 +1,31 @@
 
-puts "Wel"
+require 'pry'
+
+    def clear_screen
+        system"clear"
+    end
+
+    def welcome
+        clear_screen
+        puts "Welcome to Pokemon Battler"
+        puts "Please enter your name!"
+        username = gets.chomp
+        puts "Hello #{username}! Welcome to the world of pokémon! My name is Oak! People call me the pokémon Prof! This world is inhabited by creatures called pokémon! For some people, pokémon are pets. Others use them for fights. Myself...I study pokémon as a profession."
+        puts "Are you here to test your skills battling Pokemon?"
+        puts "Please type Yes or No"
+        battle_option = gets.chomp.downcase
+        if battle_option == "no"
+        exit
+        else
+        puts "Let's battle!"
+        end
+    end
+
+   
+        
+
+
+
+
+#binding.pry
+welcome
